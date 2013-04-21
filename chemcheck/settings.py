e0c26aeb6c40db278bbdddab612761a5b5259590
Ling Su <sl_62469430@unknown705681a6435f.att.net>
2013-04-21 17:38:26 -0500

    project update

--- chemcheck/settings.py
+++ chemcheck/settings.py
@@ -12,13 +12,25 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/sl_62469430/Documents/Django/sqlite3.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/sl_62469430/Documents/Django/sqlite.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
-    }
+    },
+
+    'entry': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/Users/sl_62469430/Documents/Django/entry_sqlite.db',
+        'USER': '',
+        'REACTION': '',
+        'TRANSFORM': '',
+        'PRODUCTS': '',
+        'CONDITIONS': '',
+        'SYNTHONS': '',
+        'DOI': '',
+        }
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,7 @@
+#!/Users/sl_62469430/Documents/Django/chemcheck/venv/bin/python
+# EASY-INSTALL-DEV-SCRIPT: 'Django==1.6.dev20130321082724','django-admin.py'
+__requires__ = 'Django==1.6.dev20130321082724'
+from pkg_resources import require; require('Django==1.6.dev20130321082724')
+del require
+__file__ = '/Users/sl_62469430/Documents/Django/chemcheck/venv/src/django/django/bin/django-admin.py'
+execfile(__file__)
